a2c8c3a41ae5003ee6fbb8b636686011a2cd6647
Larry Wang <larrywang31@gmail.com>
2025-01-28 10:32:25 -0500

    store all argv values with sscanf

--- labs/proj1/src/chain_heal.c
+++ labs/proj1/src/chain_heal.c
@@ -27,14 +27,25 @@ int main(int argc, char **argv) {
 		printf("initial_power power_reduction < input_file\n");
 		return 1;
 	}	
+
 	int init_ran, jump_ran, num_jumps, init_power;
 	double power_red;
 
 	// TODO - Read in input
-	sscanf(argv[1], "%d", &init_ran);	
+
 	// Store argv values
+	sscanf(argv[1], "%d", &init_ran);	
+	sscanf(argv[2], "%d", &jump_ran);	
+	sscanf(argv[3], "%d", &num_jumps);	
+	sscanf(argv[4], "%d", &init_power);	
+	sscanf(argv[5], "%lf", &power_red);	
+
 	printf("initial range: %d\n", init_ran);
-	
+	printf("initial range: %d\n", jump_ran);
+	printf("initial range: %d\n", num_jumps);
+	printf("initial range: %d\n", init_power);
+	printf("initial range: %lf\n", power_red);
+
 	// TODO - Print out input 
 
 	// TODO - Create Nodes into array
