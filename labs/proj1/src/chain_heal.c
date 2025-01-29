35657a7b5813b6c038124de0cabedee205950c60
Larry Wang <larrywang31@gmail.com>
2025-01-28 20:47:40 -0500

    add whileloop for reading stdin

--- labs/proj1/src/chain_heal.c
+++ labs/proj1/src/chain_heal.c
@@ -4,20 +4,12 @@
 #include <math.h>
 #include <unistd.h>
 
-/* Node struct
-			name
-			x_coord
-			y_coord
-			curr_pacification_points
-			max_pacification_points
-			prev node
-*/
-
 typedef struct Node {
-	char name[101];
-	int x_coord, y_coord;
+	char *name;
+	int x_crd, y_crd;
 	int cur_pp, max_pp;
-	struct Node *prev_Node;
+	int visited;
+	struct Node *prev;
 } Node;
 
 int main(int argc, char **argv) {
@@ -31,22 +23,47 @@ int main(int argc, char **argv) {
 	int init_ran, jump_ran, num_jumps, init_power;
 	double power_red;
 
-	// TODO - Read in input
+	/*  Store argv values with sscanf
+			REF - https://utk.instructure.com/courses/218225/pages/strings-in-c?module_item_id=5126893 */
 
-	// Store argv values
 	sscanf(argv[1], "%d", &init_ran);	
 	sscanf(argv[2], "%d", &jump_ran);	
 	sscanf(argv[3], "%d", &num_jumps);	
 	sscanf(argv[4], "%d", &init_power);	
 	sscanf(argv[5], "%lf", &power_red);	
 
-	printf("initial range: %d\n", init_ran);
-	printf("initial range: %d\n", jump_ran);
-	printf("initial range: %d\n", num_jumps);
-	printf("initial range: %d\n", init_power);
-	printf("initial range: %lf\n", power_red);
+	// TODO - Read In Input From stdin
 
-	// TODO - Print out input 
+	int node_count = 0;
+	char *line = NULL;
+	size_t size = 0;
+	Node *last_node = NULL;
+
+	while(getline(&line, &size, stdin) > -1) {
+		Node *n = (Node*)malloc(sizeof(Node));
+
+		char temp[50];			 			// Use temp string for n->name
+		sscanf(line, "%d %d %d %d %s", &n->x_crd, &n->y_crd, &n->cur_pp, &n->max_pp, temp);		
+		n->name = temp;
+		n->prev = last_node;			// Set Node links
+		last_node = n;
+
+		node_count++;
+	}
+	printf("%d %d %d %d %s\n", last_node->x_crd, last_node->y_crd, last_node->cur_pp, last_node->max_pp, last_node->name);
+	printf("Number of Nodes: %d\n", node_count);
+	free(line);
 
 	// TODO - Create Nodes into array
+
+	// Deleting all Nodes
+	Node *curr = last_node;
+	while(curr != NULL) {
+		Node *next = curr->prev;
+		free(curr);
+		curr = next;
+	}
+	// TODO - Print out input 
+
+
 }
