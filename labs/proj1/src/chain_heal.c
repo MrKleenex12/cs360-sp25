c7f655b06e367d4e7e85918a70c59717ab73df22
Larry Wang <larrywang31@gmail.com>
2025-01-29 14:19:20 -0500

    add -fsanitize=address,undefined to makefile

--- labs/proj1/src/chain_heal.c
+++ labs/proj1/src/chain_heal.c
@@ -12,7 +12,15 @@ typedef struct Node {
 	struct Node *prev;
 } Node;
 
+// Print out array
+void print_arr(Node **a, const size_t size) {
+	for(size_t i = 0; i < size; i++) {				
+		printf("%s\n", a[i]->name);
+	}
+}
+
 // DFS somehow
+
 int main(int argc, char **argv) {
 	// Error check argv values
 	if(argc != 6) {
@@ -25,54 +33,46 @@ int main(int argc, char **argv) {
 	int init_ran, jump_ran, num_jumps, init_power;
 	double power_red;
 
+
 	/*  Store argv values with sscanf
 			REF - https://utk.instructure.com/courses/218225/pages/strings-in-c?module_item_id=5126893 */
-
 	sscanf(argv[1], "%d", &init_ran);	
 	sscanf(argv[2], "%d", &jump_ran);	
 	sscanf(argv[3], "%d", &num_jumps);	
 	sscanf(argv[4], "%d", &init_power);	
 	sscanf(argv[5], "%lf", &power_red);	
 
+
 	char *tmp_line = NULL;			// For getline
 	size_t tmp_size = 0;				// For getline
+	Node *last_n = NULL; 				// previous Node for chaining Nodes
+	size_t arr_size = 0;				// Keep count of Nodes
 
-	Node *last_node = NULL; // For chaining Nodes
-	int list_size = 0;			// Keep count of Nodes
 
-	// Loop to read stdin
+	// While loop to read in stdin and Malloc Nodes 
 	// REF - Keith Scroggs talked about using getline instead of just scanf for each line
 	while(getline(&tmp_line, &tmp_size, stdin) > -1) {
 		Node *n = (Node*)malloc(sizeof(Node));
 		sscanf(tmp_line, "%d %d %d %d %s", &(n->x_crd), &(n->y_crd), &(n->cur_pp), &(n->max_pp), n->name);		
 
-		n->prev = last_node;	// Set Node links
-		// if(n->prev != NULL) {
-		// 	printf("n: %s		n->p: %s\n", n->name, n->prev->name);
-		// }
-
-		last_node = n;
-
-		list_size++;
+		n->prev = last_n;			// Set Node links
+		last_n = n;
+		arr_size++;
 	}
-	free(tmp_line);
+	free(tmp_line);					// Free temporary char*
 
-	// printf("ln: %s		ln->prev: %s\n", last_node->name, last_node->prev->name);
 
-	Node **list = (Node**)malloc(list_size * sizeof(Node));
-	// Put Nodes into list
-	for(int i = list_size-1; i >= 0; i--) {
-		list[i] = last_node;
-		last_node = last_node->prev;
+	// Malloc Node array and put Nodes in them
+	Node **arr_nodes = (Node**)malloc(arr_size * sizeof(Node));
+	for(size_t i = arr_size-1; i >= 0; i--) {			
+		arr_nodes[i] = last_n;
+		last_n = last_n->prev;
 	}
 
-	for(int i = 0; i < list_size; i++) {
-		printf("%s\n", list[i]->name);
-	}
 
 	// TODO - Make Graph
 
 	// Deleting all Nodes
-	for(int i = 0; i < list_size; i++) {	free(list[i]); }
-	free(list);
+	for(size_t i = 0; i < arr_size; i++) {	free(arr_nodes[i]); }
+	free(arr_nodes);
 }
