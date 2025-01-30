7eb979673bc09b238f4b050d45f8ea238bc3a5ce
Larry Wang <larrywang31@gmail.com>
2025-01-30 10:48:37 -0500

    start create_adj function

--- labs/proj1/src/chain_heal.c
+++ labs/proj1/src/chain_heal.c
@@ -8,17 +8,53 @@ typedef struct Node {
 	char name[101];
 	int x_crd, y_crd;
 	int cur_pp, max_pp;
-	int visited;
+	int visited, adj_size;
 	struct Node *prev;
+	struct NOde **adj;
 } Node;
 
 // Print out array
-void print_arr(Node **a, const size_t size) {
-	for(size_t i = 0; i < size; i++) {				
-		printf("%s\n", a[i]->name);
+void print_arr(Node **arr, const size_t arr_size) {
+	for(size_t i = 0; i < arr_size; i++) {				
+		printf("%s\n", arr[i]->name);
 	}
 }
 
+void create_adj(Node **arr, const size_t size, const int jump_range) {
+	int dist_squared = jump_range * jump_range;
+	/*	Initialize adjacency graph to keep track of connections
+			REF - https://www.techiedelight.com/initialize-2d-array-with-zeroes-c/ */
+	int grid_2d[size][size];
+	memset(grid_2d, 0, sizeof(grid_2d));		// set every value as 0
+
+	// Find connections with jump_range
+	for(size_t i = 0; i < size - 1; i++) {
+		Node *n1 = arr[i];
+		for(size_t j = i+1; j < size; j++) {
+			Node *n2 = arr[j];
+			int x_diff = n2->x_crd - n1->x_crd;
+			int y_diff = n2->y_crd - n1->y_crd;
+			int total = (x_diff * x_diff) + (y_diff * y_diff);
+			if(total > dist_squared) { continue; }
+
+			// Adjust Node's adjacency size and adjacency graph
+			n1->adj_size++;
+			n2->adj_size++;
+			grid_2d[i][j] = 1;
+			grid_2d[j][i] = 1;
+		}
+	}
+
+	// /* 
+	for(size_t i = 0; i < size; i++) {
+		for(size_t j = 0; j < size; j++) {
+			printf("%d ", grid_2d[i][j]);
+		}
+		printf("\n");
+	}
+	// */
+	
+}
 // DFS somehow
 
 int main(int argc, char **argv) {
@@ -55,22 +91,27 @@ int main(int argc, char **argv) {
 		Node *n = (Node*)malloc(sizeof(Node));
 		sscanf(tmp_line, "%d %d %d %d %s", &(n->x_crd), &(n->y_crd), &(n->cur_pp), &(n->max_pp), n->name);		
 
+		n->adj_size = 0;
+		n->visited = 0;
 		n->prev = last_n;			// Set Node links
 		last_n = n;
 		arr_size++;
 	}
 	free(tmp_line);					// Free temporary char*
 
+	// printf("ln: %s		ln->p: %s\n", last_n->name, last_n->prev->name);
 
 	// Malloc Node array and put Nodes in them
 	Node **arr_nodes = (Node**)malloc(arr_size * sizeof(Node));
-	for(size_t i = arr_size-1; i >= 0; i--) {			
+	for(int i = arr_size-1; i >= 0; i--) {			
 		arr_nodes[i] = last_n;
 		last_n = last_n->prev;
 	}
 
+	// print_arr(arr_nodes, arr_size);
 
 	// TODO - Make Graph
+	create_adj(arr_nodes, arr_size, jump_ran);
 
 	// Deleting all Nodes
 	for(size_t i = 0; i < arr_size; i++) {	free(arr_nodes[i]); }
