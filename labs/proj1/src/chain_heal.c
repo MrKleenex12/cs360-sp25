f7a4d7cfee00116d1f4efe357eef9c239c565caf
Larry Wang <larrywang31@gmail.com>
2025-02-01 14:03:17 -0500

    rewrote structs and got correct input

--- labs/proj1/src/chain_heal.c
+++ labs/proj1/src/chain_heal.c
@@ -8,16 +8,21 @@ typedef size_t ST;
 
 typedef struct Node {
 	char name[101], is_start, visited;
+	int id;
 	int X, Y, cur_pp, max_pp, max_heal;
 	ST adj_size;
 	struct Node *prev, **adj;
 } Node;
 
 
-typedef struct Global {
-	int jumps, init_heal, best;
-	double heal_scale;
-	Node *last;
+// Command Line values
+typedef struct Command_Line_Variables {
+	int init_ran, jump_ran, jumps, init_power;
+	double scale;
+} CL;
+
+typedef struct Global_Variables {
+	int best_heal, *heal_arr, *path_arr;
 } Global;
 
 // ****************************** FUNCTIONS ****************************** 
@@ -61,6 +66,7 @@ Node** make_array(const ST size, Node **last_node) {
 	Node **arr = (Node**)malloc(size * sizeof(Node));
 	for(int i = size-1; i >= 0; i--) {			
 		arr[i] = *last_node;
+		(*last_node)->id = i;
 		*last_node = (*last_node)->prev;
 	}
 
@@ -129,26 +135,37 @@ void find_start_Nodes(Node **arr, const ST size, const int init_ran) {
 
 }
 // Caclulate healing on current Node
-int calc_healing(Node *curr, const int hop_num, Global *v) {
-	int h = rint( (double) v->init_heal * pow(v->heal_scale, hop_num-1));
+int calc_healing(Node **current, const int hop_num, CL *cl) {
+	Node *curr = *current;
+	int h = rint( (double) cl->init_power * pow(cl->scale, hop_num-1));
 	curr->max_heal = curr->max_pp - curr->cur_pp;
-	if(h > curr->max_heal) {		h = curr->max_heal;		}
-	else {curr->max_heal = h; }
+	if(h > curr->max_heal) {
+		h = curr->max_heal;
+	}		else {
+		curr->max_heal = h;
+	}
 	return h;
 }
 
-void dfs_rec(Node *curr, const int hop_num, int healing, Global **vars) {
-	Global *v = *vars;	
+void dfs_rec(Node *curr, const int hop_num, int healing, CL *cl, Global **vars) {
+	Global *dv = *vars;
 	// BASE CASE - MAX JUMPS
-	if(hop_num > v->jumps) { return;	}
+	if(hop_num > cl->jumps) { return;	}
+	// printf("%s(%d) - Hop: %d\n", curr->name, curr->id, hop_num);
 
 	curr->visited = 1;											// Set current Node as visited
 	// Caclulate Healing
-	healing += calc_healing(curr, hop_num, *vars);
-	if(healing > v->best) {
-		(*vars)->best = healing;
-		// Update last Node of longest healing
-		(*vars)->last = curr;
+	healing += calc_healing(&curr, hop_num, cl);
+	if(healing > dv->best_heal) {
+		dv->best_heal = healing;
+		// printf("\nNew Best Healing: %d\n\n", healing);
+
+		Node *index = curr;
+		for(int i = hop_num-1; i >= 0; i--) {
+			dv->path_arr[i] = index->id;
+			dv->heal_arr[i] = index->max_heal;
+			index = index->prev;
+		}
 	}
 	// Index through neighbors of curr Node and call dfs_rec
 	for(ST i = 0; i < curr->adj_size; i++) {
@@ -157,17 +174,22 @@ void dfs_rec(Node *curr, const int hop_num, int healing, Global **vars) {
 		if(1 == index->visited) { continue; }
 
 		index->prev = curr;
-		dfs_rec(index, hop_num+1, healing, vars);
+		// printf("	%s %d -> %s\n", curr->name, hop_num, index->name);
+		dfs_rec(index, hop_num+1, healing, cl, vars);
 		index->visited = 0;										// Set as unvisited when backtracking
 	}
 }
 
-void DFS(Node **arr, ST size, Global *vars) {
+void DFS(Node **arr, ST size, CL *cl, Global **vars) {
+	Global *dv = *vars;
+	dv->path_arr = (int*)malloc(cl->jumps * sizeof(int));
+	dv->heal_arr = (int*)malloc(cl->jumps * sizeof(int));
+
 	for(ST i = 0; i < size; i++) {
 		Node *n = arr[i];
 		if(0 == n->is_start ) {	continue; }
 		
-		dfs_rec(n, 1, 0, &vars);
+		dfs_rec(n, 1, 0, cl, vars);
 	}
 }
 
@@ -182,39 +204,32 @@ int main(int argc, char **argv) {
 
 	/*  Store argv values with sscanf
 			REF - https://utk.instructure.com/courses/218225/pages/strings-in-c?module_item_id=5126893 */
-	int init_ran, jump_ran, num_jumps, init_power;
-	double power_red;
-	sscanf(argv[1], "%d", &init_ran);	
-	sscanf(argv[2], "%d", &jump_ran);	
-	sscanf(argv[3], "%d", &num_jumps);	
-	sscanf(argv[4], "%d", &init_power);	
-	sscanf(argv[5], "%lf", &power_red);	
-
+	// Store global variables in a struct
+	CL *cl = (CL*)malloc(sizeof(CL));
+	sscanf(argv[1], "%d", &(cl->init_ran));	
+	sscanf(argv[2], "%d", &(cl->jump_ran));	
+	sscanf(argv[3], "%d", &(cl->jumps));	
+	sscanf(argv[4], "%d", &(cl->init_power));	
+	sscanf(argv[5], "%lf", &(cl->scale));	
+	cl->scale = 1.0 - cl->scale;
 	
 	ST size = 0;															// Node counter
 	Node *last_n = read_stdin(&size);					// Last Node in the list 
 	Node **arr = make_array(size, &last_n);		// Allocate and fill Node array
-	make_adj_lists(arr, size, jump_ran);			// Allocate memory & make adjacency list;
-	find_start_Nodes(arr, size, init_ran); 		// Starting Nodes will be checked off 
-	// Store global variables in a struct
-	Global vars = {num_jumps, init_power,0, 1.0-power_red, NULL};
-	DFS(arr, size, &vars);											// Initial call for DFS
+	make_adj_lists(arr, size, cl->jump_ran);			// Allocate memory & make adjacency list;
+	find_start_Nodes(arr, size, cl->init_ran); 		// Starting Nodes will be checked off 
+	Global *vars = (Global*)malloc(sizeof(Global));
+	DFS(arr, size, cl, &vars);											// Initial call for DFS
 
-	Node *index = vars.last;
-	Node **tmp_arr = (Node**)malloc(num_jumps * sizeof(Node));
-	for(int i = num_jumps-1; i >= 0; i--) {
-		tmp_arr[i] = index;
-		index = index->prev;
+	for(int i = 0; i < cl->jumps; i++) {
+		int index = vars->path_arr[i];
+		printf("%s %d\n", arr[index]->name, vars->heal_arr[i]);
 	}
+	printf("Total_Healing %d\n", vars->best_heal);
 
-	// OUTPUT
-	for(ST i = 0; i < (ST)num_jumps; i++) {
-		printf("%s %d\n", tmp_arr[i]->name,tmp_arr[i]->max_heal);
-	}
-	free(tmp_arr);
-	printf("Total_Healing %d\n", vars.best);
-
-	// Deleting Nodes and other memory
+	// Freeing memory
+	free(vars->heal_arr);
+	free(vars->path_arr);
 	for(ST i = 0; i < size; i++) {	
 		free(arr[i]->adj);
 		free(arr[i]); 
