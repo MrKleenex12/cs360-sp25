a81ae993ed84ada38380be84b431b3176283ae7c
Larry Wang <larrywang31@gmail.com>
2025-02-08 18:11:42 -0500

    add Person structs and get names

--- labs/proj2/src/famtree.c
+++ labs/proj2/src/famtree.c
@@ -13,15 +13,58 @@
     List of children
 */
 
-int read_file(const char* filename, IS *input) {
-  *input = new_inputstruct(filename);
-  IS is = *input;
-  if(NULL == is) {  return 1; }
+typedef struct person {
+  char *name, *dad, *mom, sex;
+} Person;
 
-  while(get_line(is) >= 0) {
-    printf("%s", is->text1); 
+char* full_name(IS is) {
+  int nf = is->NF;
+  int ssize = strlen(is->fields[1]);
+  /* Adjust size if more words in name */
+  for(int i = 2; i < nf; i++) { ssize += (strlen(is->fields[i]) + 1); }
+
+  /* Get first word */
+  char* name = (char*)malloc((ssize+1) * sizeof(char)); 
+  strcpy(name, is->fields[1]);
+
+  /* Get rest of words */
+  ssize = strlen(is->fields[1]);
+  for(int i = 2; i < nf; i++) {
+    name[ssize] = ' ';
+    strcpy(name+ssize+1, is->fields[i]);
+    ssize += strlen(name+ssize);
   }
 
+  return name;
+}
+
+char* one_name(IS is) {
+  char* name = (char*)malloc((strlen(is->fields[1]) + 1) * sizeof(char)); 
+  strcpy(name, is->fields[1]);
+  return name;
+}
+
+int read_file(const char* filename) {
+  IS is = new_inputstruct(filename);
+  if(NULL == is) {  return 1; }       /* Quit if reading argv went wrong */
+
+  /* Parse through file */
+  int count = 0;
+  while(get_line(is) >= 0) {
+    if(strcmp(is->fields[0], "PERSON") == 0) { 
+      // Create Person with just name like in /testing
+      Person *p = (Person*)malloc(sizeof(Person));
+
+      p->name = (is->NF > 2) ? full_name(is) : one_name(is);
+
+      free(p->name);
+      free(p);
+      count++; 
+    }
+  }
+  printf("There are %d of People in %s\n", count, filename);
+
+  jettison_inputstruct(is);
   return 0;
 }
 
@@ -33,12 +76,8 @@ int main(int argc, char **argv) {
   }
 
   /* Check valid file */
-  IS is;
-  if(1 == read_file(argv[1], &is)) {
+  if(1 == read_file(argv[1])) {
     perror(argv[1]);
     return 1; 
   };
-
-  
-  jettison_inputstruct(is);
 }
