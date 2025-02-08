21632f53be6c48e369183f3b08b84ca725151f67
Larry Wang <larrywang31@gmail.com>
2025-02-08 16:22:14 -0500

    create function to read input

--- labs/proj2/src/famtree.c
+++ labs/proj2/src/famtree.c
@@ -1,3 +1,44 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dllist.h" 
+#include "fields.h"
+#include "jrb.h"
+
+/*  Make Person struct
+    name
+    Sex
+    Father
+    Mother
+    List of children
+*/
+
+int read_file(const char* filename, IS *input) {
+  *input = new_inputstruct(filename);
+  IS is = *input;
+  if(NULL == is) {  return 1; }
+
+  while(get_line(is) >= 0) {
+    printf("%s", is->text1); 
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  /* Check Usage */
+  if(2 != argc) {
+    fprintf(stderr, "usage: bin/famtree filenames\n");
+    exit(1);
+  }
+
+  /* Check valid file */
+  IS is;
+  if(1 == read_file(argv[1], &is)) {
+    perror(argv[1]);
+    return 1; 
+  };
+
   
+  jettison_inputstruct(is);
 }
