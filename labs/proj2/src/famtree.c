5f7df25ed1fa19d62d4aceee13af1b9690156c5b
Larry Wang <larrywang31@gmail.com>
2025-02-09 21:11:57 -0500

    add basic dfs and update sex print

--- labs/proj2/src/famtree.c
+++ labs/proj2/src/famtree.c
@@ -8,6 +8,7 @@
 
 typedef struct person {
   char *name, sex;
+  char visited;
   struct person *dad, *mom;
   Dllist kid_list;
 } Person;
@@ -18,11 +19,9 @@ char* full_name(IS is) {
   int ssize = strlen(is->fields[1]);
   /* Adjust size if more words in name */
   for(int i = 2; i < nf; i++) { ssize += (strlen(is->fields[i]) + 1); }
-
   /* Get first word */
   char* name = (char*)malloc((ssize+1) * sizeof(char)); 
   strcpy(name, is->fields[1]);
-
   /* Get rest of words */
   ssize = strlen(is->fields[1]);
   for(int i = 2; i < nf; i++) {
@@ -53,13 +52,14 @@ Person* create_person(char* name, JRB *tree) {
   p->dad = NULL;
   p->mom = NULL;
   p->sex = 'U';
+  p->visited = 0;
   p->kid_list = new_dllist();
   jrb_insert_str(*tree, name, new_jval_v((void*) p));
 
   return p;
 }
 
-void free_person(Person *p) {
+void free_person(Person* p) {
   free(p->name);
   free_dllist(p->kid_list);
   free(p);
@@ -68,22 +68,19 @@ void free_person(Person *p) {
 void print(Person* p) {
   printf("%s\n", p->name);
 
-  printf("  Sex: ");
-  if(p->sex != 'U') { printf("%c\n", p->sex); }
-  else { printf("Unknown\n"); }
-  printf("  Father: ");
-  if(p->dad != NULL) { printf("%s\n", p->dad->name); }
-  else { printf("Unknown\n"); }
-  printf("  Mother: ");
-  if(p->mom != NULL) { printf("%s\n", p->mom->name); }
-  else { printf("Unknown\n"); }
+  char *sex = (p->sex == 'M') ? "Male" : "Female";
+  if(p->sex != 'U') { printf("  Sex: %s\n", sex); }
+  else { printf("  Sex: Unknown\n"); }
+  if(p->dad != NULL) { printf("  Father: %s\n", p->dad->name); }
+  else { printf("  Father: Unknown\n"); }
+  if(p->mom != NULL) { printf("  Mother: %s\n", p->mom->name); }
+  else { printf("  Mother: Unknown\n"); }
 
   if(dll_empty(p->kid_list)) { printf("  Children: None\n"); }
   else {
     Dllist tmp;
     dll_traverse(tmp, p->kid_list) {
-      Person* p2 = (Person*)tmp->val.v;
-      printf("  Children: %s\n", p2->name);
+      printf("  Children: %s\n", ((Person*)tmp->val.v)->name);
     }
   }
 }
@@ -97,37 +94,26 @@ void add_kid(Person* p1, Person* p2) {
   dll_append(p1->kid_list, new_jval_v((void*) p2));
 }
 
-int read_stdin(JRB *tree) {
+void read_stdin(JRB *tree) {
   IS is = new_inputstruct(NULL);
-  if(NULL == is) {  return 1; }                   /* Quit if reading argv went wrong */
   Person *p1, *p2;
 
   /* Parse through file with while loop*/
   while(get_line(is) >= 0) { 
     if(is->NF == 0) { continue; }                 /* Skip blank lines */
     char *name = (is->NF > 2) ? full_name(is) : one_name(is);
-
-    if(strcmp(is->fields[0], "PERSON") == 0) {    /* Update p1 if needed*/
+    /* Update p1 if needed*/
+    if(strcmp(is->fields[0], "PERSON") == 0) {
       p1 = create_person(name, tree);
       continue;
     }
-
     // Update p2 if not SEX line
     if(strcmp(is->fields[0], "SEX") != 0) { p2 = create_person(name, tree); }
-
     /* Relational Links*/
-    if(strcmp(is->fields[0], "FATHER") == 0) {
-      add_parent(p1, p2, 'M');
-    }
-    else if(strcmp(is->fields[0], "MOTHER") == 0) {
-      add_parent(p1, p2, 'F');
-    }
-    else if(strcmp(is->fields[0], "FATHER_OF") == 0) {
-      add_kid(p1, p2);
-    }
-    else if(strcmp(is->fields[0], "MOTHER_OF") == 0) {
-      add_kid(p1, p2);
-    }
+    if(strcmp(is->fields[0], "FATHER") == 0) { add_parent(p1, p2, 'M'); }
+    else if(strcmp(is->fields[0], "MOTHER") == 0) { add_parent(p1, p2, 'F'); }
+    else if(strcmp(is->fields[0], "FATHER_OF") == 0) { add_kid(p1, p2); }
+    else if(strcmp(is->fields[0], "MOTHER_OF") == 0) { add_kid(p1, p2); }
     else if(strcmp(is->fields[0], "SEX") == 0) {
       p1->sex = *(is->fields[1]);
       free(name);
@@ -135,22 +121,40 @@ int read_stdin(JRB *tree) {
   }
 
   jettison_inputstruct(is);
+}
+
+int check_cycle(Person* p, Dllist tmp) {
+  if(p->visited == 1) { return 1; }
+  p->visited = 1;
+  dll_traverse(tmp, p->kid_list) {
+    Person* p = ((Person*)tmp->val.v);
+    check_cycle(p, tmp);
+    p->visited = 0;
+  }
+
   return 0;
 }
 
 int main(int argc, char **argv) {
   /* Read from stdin */
   JRB tree = make_jrb();
-  if(1 == read_stdin(&tree)) {
-    perror(argv[1]);
-    return 1; 
-  };
+  read_stdin(&tree);
 
+  Dllist index;
   JRB tmp;
+
   jrb_traverse(tmp, tree) {
     Person* p = (Person*) tmp->val.v;
     print(p);
+    /*
+    if(check_cycle(p, index) == 1) {
+      fprintf(stderr, "Cycle Detected\n");
+      return 1;
+    }
+    */
   }
+
+  /* Freeing Everything*/
   jrb_traverse(tmp, tree) {
     free_person((Person*)tmp->val.v);
   }
