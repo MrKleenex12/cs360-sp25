85821203733e21d1c2511d44186a7c25f5409feb
Larry Wang <larrywang31@gmail.com>
2025-02-16 20:25:57 -0500

    commented out stuff for debugging

--- labs/proj3/src/huff_dec.c
+++ labs/proj3/src/huff_dec.c
@@ -27,7 +27,7 @@ off_t get_file_size(const char* file_name) {
 }
 
 /* last 4 bytes indicate how many bits should be read */
-uint32_t last_four(const char* file_name, const off_t fsize) {
+u_int32_t last_four(const char* file_name, const off_t fsize) {
   FILE* f = fopen(file_name, "r");
   if(f == NULL) {   return 1;   }       /* Fail if can't read file */ 
 
@@ -41,67 +41,78 @@ uint32_t last_four(const char* file_name, const off_t fsize) {
   return nbits;
 }
 
-void delete_tree(HN* node) {
-  if(node == NULL) { return; }
+void delete_tree(HN* n) {
+  if(n == NULL) { return; }
 
-  delete_tree(node->ptrs[0]);
-  delete_tree(node->ptrs[1]);
+  delete_tree(n->ptrs[0]);
+  delete_tree(n->ptrs[1]);
 
-  if(node->strings[0] != NULL) { free(node->strings[0]); }
-  if(node->strings[1] != NULL) { free(node->strings[1]); }
-  free(node);
+  if(n->strings[0] != NULL) { free(n->strings[0]); }
+  if(n->strings[1] != NULL) { free(n->strings[1]); }
+  free(n);
 }
 
-char* read_string(const char* buff, size_t *curr, size_t *last) {
+void print(HN* n) {
+  if(n->strings[0] != NULL) {
+    printf("0:%8s\n", n->strings[0]);
+  }
+  if(n->strings[1] != NULL) {
+    printf("0:%8s\n", n->strings[1]);
+  }
+
+  if(n->ptrs[0] != NULL) { print(n->ptrs[0]); }
+  if(n->ptrs[1] != NULL) { print(n->ptrs[1]); }
+}
+
+char* read_string(const char* buff, int *curr, int *last) {
   while(buff[*curr] != 0) {   (*curr)++;    }
 
   int len = *curr - *last + 1;
   char *s = (char*)malloc(len * sizeof(char));
   snprintf(s, len, "%s", buff + *last);
-  *last = ++(*curr);
 
   return s;
 }
 
-void add_to_tree(HN* head, const char* buff, char* str, size_t* curr) {
-  HN* last_hn = head;
+void add_to_tree(HN** head, char* str, const char* buff, int* curr) {
+  // HN* last_hn = *head;
   int reading = buff[*curr]-48;
   printf("%d ", reading);
   (*curr)++;
 
   while(buff[*curr] != 0) {
+    /*
     HN* child = last_hn->ptrs[reading];
-
     if(child == NULL) { child = create_hn(); }
     last_hn = child; 
-    
+    */
     reading = buff[*curr]-48;
-
-    printf("%d ", buff[*curr]-48);
+    printf("%d ", reading);
     (*curr)++;
   }
 
-  last_hn->strings[reading] = str;
+  // last_hn->strings[reading] = str;
+  // printf("%s\n", last_hn->strings[reading]);
   printf("\n");
 
 }
 
-int open_code_file(const char* file_name, const off_t fsize, HN* head) {
+int open_code_file(const char* file_name, const off_t fsize, HN** head) {
   FILE* f = fopen(file_name, "rb");  // Open in binary mode to avoid issues with line endings
   if (f == NULL) {   return 1;   }
   char buff[fsize];
-  size_t nobjects;
+  int nobjects;
   
   while ((nobjects = fread(buff, 1, sizeof(buff), f)) > 0) {
-    size_t curr = 0;
-    size_t last = 0;
-    while(curr < nobjects) {
-      char* s = read_string(buff, &curr, &last);    /* Read string */
-      // printf("string: %s \n", str);
-      free(s);
+    int curr_index = 0;
+    int last_index = 0;
 
-      add_to_tree(head, buff, s, &curr);               /* Read Sequence of bits */
-      last = ++curr;
+    while(curr_index < nobjects) {
+      char* s = read_string(buff, &curr_index, &last_index);    /* Read string */
+      last_index = ++curr_index;
+      printf("string: %s \n", s);
+      add_to_tree(head, s, buff, &curr_index);               /* Read Sequence of bits */
+      last_index = ++curr_index;
     }
   }
 
@@ -116,9 +127,20 @@ int main(int argc, char** argv) {
     return 1;
   }
 
+  /*
+  HN* test = create_hn();
+  char* name = malloc(6 * sizeof(char));
+  strcpy(name, "Larry");
+  test->strings[0] = name;
+  // print(test);
+  free(test);
+  free(name);
+  */
 
   HN* head = create_hn();
-  open_code_file(argv[1], file_size, head);
+  open_code_file(argv[1], file_size, &head);
+  print(head);
+
   // printf("%10lld - %s\n", file_size, argv[1]);
   // int nbits = last_four(argv[1], file_size);
   // printf("number of bits:  %d\n", nbits);
