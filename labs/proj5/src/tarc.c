2cb199baf943b29ccb95a6fde9de4b31ee6d79b6
Larry Wang <larrywang31@gmail.com>
2025-03-02 01:57:45 -0500

    raa

--- labs/proj5/src/tarc.c
+++ labs/proj5/src/tarc.c
@@ -12,85 +12,90 @@
 #include "sys/stat.h"
 
 #define BUF_SIZE 8192
-#define ARR_SIZE 10
+#define PATH_MAX 512 
+#define INIT_ARR_SIZE 4 
 
 typedef struct string_vector {
-  int len, size;
-  char **arr;
+  int length;   /* Total Length Allocated */
+  int size;     /* Size of items in vector */
+  char **vector;
 } SV;
 
 void free_SV(SV *sv) {
-  for(int i = 0; i < sv->size; i++) {
-    if(sv->arr[i] != NULL) free(sv->arr[i]);
-  }
-  free(sv->arr);
+  for(int i = 0; i < sv->size; i++) { free(sv->vector[i]); }
+  free(sv->vector);
   free(sv);
 }
 
 SV* new_SV() {
   SV *sv = (SV*)malloc(sizeof(SV));
-  sv->arr = (char**)malloc(ARR_SIZE * sizeof(char*));
-  sv->len = ARR_SIZE;
+  sv->vector = (char**)malloc(INIT_ARR_SIZE * sizeof(char*));
+  sv->length = INIT_ARR_SIZE;
   sv->size = 0;
 
   return sv;
 }
 
 void SV_append(SV *sv, char *str) {
-  if(sv->size == sv->len) {
-    sv->len += sv->len/2;
-    char **temp = (char**)realloc(sv->arr, sv->len * sizeof(char*));
+  /* Resize if vector full */
+  if(sv->size == sv->length) {
+    /* 1.5 times larger */
+    sv->length += sv->length/2;
+    /* Reallocate space for bigger vector */
+    char **temp = (char**)realloc(sv->vector, sv->length * sizeof(char*));
     if(temp == NULL) {
       fprintf(stderr, "Realloc Failed\n");
       exit(1);
     }
-    sv->arr = temp;
+    sv->vector = temp;
   } 
 
-  sv->arr[sv->size++] = str;
+  sv->vector[sv->size++] = strdup(str);  /* Append string to vector */
 }
 
-void dir_dfs(const char *dir_name) {
+void read_dir(DIR *d, SV *sv, const char *dir_name) {
+  struct dirent *file;
+  struct stat buf;
+  char path[PATH_MAX];
+
+  while((file = readdir(d)) != NULL) {
+    char *f = file->d_name;
+    /* skip if . or .. directory */
+    if(strcmp(f, ".") == 0 || strcmp(f, "..") == 0) continue; 
+    /* Add file to path name */
+    snprintf(path, sizeof(path), "%s/%s", dir_name, f);
+
+    if(stat(path, &buf) < 0) {      /* read in filename */
+      perror(path);
+      exit(1);
+    }
+
+    /* If directory, add to vector to check later*/
+    if(S_ISDIR(buf.st_mode)) { SV_append(sv, path); }
+    else { printf("FILE: %s\n", path); }
+  }
+}
+
+void open_dir(const char *dir_name) {
+  printf("DIR: %s\n", dir_name);
   DIR *d = opendir(dir_name);
   if(d == NULL) {
     perror(dir_name);
     exit(1);
   }
 
-  struct dirent *de;
-  struct stat buf;
-  int exists;
-  SV *sv = new_SV();  /* string vector */
+  SV *str_vec = new_SV();           /* Hold names of directories */
+  read_dir(d, str_vec, dir_name);   /* Print out files in directory */
+  closedir(d);                      /* Close current directory */
 
-  while((de = readdir(d)) != NULL) {
-    /* n is name of current item in directory */
-    char *n = de->d_name;
-    /* skip if . or .. directory */
-    if(strcmp(n, ".") == 0 || strcmp(n, "..") == 0) continue; 
-
-    /* read in file/directory name */
-    if(stat(n, &buf) < 0) {
-      perror(n);
-      fprintf(stderr, "Error reading in %s\n", n);
-      exit(1);
-    }
-
-    if(S_ISDIR(buf.st_mode)) { SV_append(sv, strdup(n)); }  /* IF dir, add to vector to check later*/
-    else { printf("FILE: %s\n", n); }               /* Print out file */
-
+  /* Recursively print files in other directories */
+  for(int i = 0; i < str_vec->size; i++) {
+    open_dir(str_vec->vector[i]);
   }
-  
-  closedir(d);
-  /* Index through directories*/
-  for(int i = 0; i < sv->size; i++) {
-    printf("%s\n", sv->arr[i]);
-    dir_dfs(sv->arr[i]);
-  }
-
-  free_SV(sv);
+  free_SV(str_vec);
 }
 
 int main(int argc, char **argv) {
-  dir_dfs(".");
+  open_dir(argv[1]);
   return 0;
 }
