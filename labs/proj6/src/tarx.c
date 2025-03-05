0da19b42b1d10e80c232efbd834e17ea2e0f6a22
Larry Wang <larrywang31@gmail.com>
2025-03-05 15:16:25 -0500

    testing

--- labs/proj6/src/tarx.c
+++ labs/proj6/src/tarx.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <jrb.h>
 #include <jval.h>
 
@@ -24,29 +25,35 @@ void read_tar() {
   long ltmp, in;
   u_int32_t itmp; 
   char buffer[BUF_SIZE];
+  char name[BUF_SIZE];
 
   JRB inodes, tmp;
   inodes = make_jrb();
-  // size_t nobjects;
 
   while(fread(&itmp, 4, 1, stdin) > 0) {
+    fread(&itmp, 4, 1, stdin);
     /* All files */
-    fread(buffer, itmp, 1, stdin);
-    buffer[itmp] = '\0';
-    printf("Name: %s\n", buffer);
+    fread(name, itmp, 1, stdin);
+    name[itmp] = '\0';
+    printf("Name: %s\n", name);
     fread(&in, 8, 1, stdin);
     printf("Inode %ld ", in);
     /* IF first time seeing inode */
     tmp = jrb_find_dbl(inodes, in);
     if(tmp == NULL) {
       fread(&itmp, 4, 1, stdin);
-      printf("Mode %u ", itmp);
+      printf("Mode %o ", itmp);
       fread(&ltmp, 8, 1, stdin);
       printf("Mtime %ld", ltmp);
 
       if((itmp >> 15 & 1) == 1) {
         fread(&ltmp, 8, 1, stdin);
         fread(buffer, ltmp, 1, stdin);
+      } else {
+        strcpy(buffer, name);
+        printf("buffer: %s\n", buffer);
+        snprintf(name, BUF_SIZE, "mkdir %s", buffer);
+        system(name);
       }
 
       /* Add into list after */
