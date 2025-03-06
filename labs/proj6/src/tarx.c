f0bbcfd2c7b96798ea06c1a3fa517949d904b6ce
Larry Wang <larrywang31@gmail.com>
2025-03-06 14:03:00 -0500

    make everything without permissions

--- labs/proj6/src/tarx.c
+++ labs/proj6/src/tarx.c
@@ -14,8 +14,8 @@
 
 typedef struct char_buffers {
   char buffer[BUF_SIZE];
-  char name[BUF_SIZE];
-} Cbufs;
+  char fname[BUF_SIZE];
+} CBufs;
 
 typedef struct byte_sizes {
   long in, mtime, fsize;
@@ -24,92 +24,88 @@ typedef struct byte_sizes {
 
 typedef struct timeval timeval;
 
-/*  Order of bytes:
-
-  - size of filename  - 4B int
-  - file's name without NULL char
-  - file's inode      - 8B long
-
-  IF FIRST TIME READING
-  - file's mode       - 4B int
-  - file's mod time   - 8B long
-
-    IF FILE & NOT DIR
-    - file's size     - 8B long
-    - file's bytes (contents)
-*/
-int slash(const char *name) {
-  for(int i = strlen(name)-1; i >= 0; i--) {
-    if(name[i] == '/') { return i+1; }
+int slash(const char *fname) {
+  for(int i = strlen(fname)-1; i >= 0; i--) {
+    if(fname[i] == '/') { return i+1; }
   }
   return 0;
 }
 
-void set_time(char *name, timeval *times, long *mtime) {
+void set_time(char *fname, timeval *times, long *mtime) {
   times[0].tv_usec = 0;
   times[0].tv_sec = time(0);
   times[1].tv_usec = 0;
   times[1].tv_sec = *mtime;
-  utimes(name, times);
+  utimes(fname, times);
 }
 
-void general_info(char *name, u_int32_t *itmp, long *in) {
-  /* All files */
-  fread(name, *itmp, 1, stdin);                 /* Size of filename */
+void all_info(char *fname, u_int32_t *itmp, long *in) {
+  fread(fname, *itmp, 1, stdin);                /* Size of filename */
   fread(in, 8, 1, stdin);                       /* Name of file */
-
-  name[*itmp] = '\0';
-  
-  printf("Name: %s\n", name+slash(name));
-  printf("Inode %ld ", *in);
+  fname[*itmp] = '\0';
+  // printf("Name: %s\n", fname);
+  // printf("Inode %ld ", *in);
 }
 
-/* TODO Cleanup function? */
-void first_read(Cbufs *cb, BSizes *bs, timeval *times) {
+void create_file(CBufs *cb, BSizes *bs) {
+  int fd, result;
+
+  creat(cb->fname, 33188);
+  fd = open(cb->fname, O_WRONLY);
+  result = write(fd, cb->buffer, bs->fsize);
+  close(fd);
+}
+
+void first_read(CBufs *cb, BSizes *bs, timeval *times) {
   /* Read mode and Mtime */
   fread(&(bs->itmp), 4, 1, stdin);              /* Mode */
   fread(&(bs->mtime), 8, 1, stdin);             /* Mtime */
-  printf("Mode %o ", bs->itmp);
-  printf("Mtime %ld", bs->mtime);
+  // printf("Mode %o ", bs->itmp);
+  // printf("Mtime %ld", bs->mtime);
 
-  /* IF File and not Directory */
+  /* IF File */
   if(((bs->itmp) >> 15 & 1) == 1) {
     fread(&(bs->fsize), 8, 1, stdin);           /* Size of file */
     fread(cb->buffer, (bs->fsize), 1, stdin);   /* File's contents */
-
-    /* Create file and put contents in it */
-    creat(cb->name, bs->itmp);
-    int fd = open(cb->name, O_WRONLY);
-    int result = write(fd, cb->buffer, bs->fsize);
-    close(fd);
+    create_file(cb, bs);                        /* Create file & contents */
   } 
   else {
     /* TODO Make sure write protection is set after all subdirectories and files are created */
-    mkdir(cb->name, (bs->itmp));
+    mkdir(cb->fname, 16877);
   }
-  set_time(cb->name, times, &(bs->mtime));
+
+  set_time(cb->fname, times, &(bs->mtime));
+}
+
+void if_not_read(CBufs *cb, BSizes *bs, JRB inodes, JRB tmp, timeval *times) {
+  tmp = jrb_find_dbl(inodes, bs->in);
+  if(tmp != NULL) {
+    link(tmp->val.s, cb->fname);
+    return;
+  }
+
+  first_read(cb, bs, times);
+  /* Add into list after */
+  Jval j = new_jval_s(strdup(cb->fname));
+  jrb_insert_dbl(inodes, bs->in, j);                    
 }
 
 void read_tar() {
-  Cbufs cb;
+  CBufs cb;
   BSizes bs;
   JRB inodes, tmp;
   inodes = make_jrb();
   timeval times[2];
 
+  /* Continously read through all of stdin */
   while(fread(&bs.itmp, 4, 1, stdin) > 0) {
-    general_info(cb.name, &bs.itmp, &bs.in);
-
-    /* IF first time seeing inode */
-    tmp = jrb_find_dbl(inodes, bs.in);
-    if(tmp == NULL) {
-      first_read(&cb, &bs, times);                         /* Read in mode and mtime */
-      jrb_insert_dbl(inodes, bs.in, new_jval_i(0)); /* Add into list after */
-    }
-
-    printf("\n\n");
+    all_info(cb.fname, &bs.itmp, &bs.in);
+    if_not_read(&cb, &bs, inodes, tmp, times);
+    // printf("\n\n");
   }
 
+  /* Free JRB */
+  jrb_traverse(tmp, inodes) { free(tmp->val.s); }
   jrb_free_tree(inodes);
 }
 
