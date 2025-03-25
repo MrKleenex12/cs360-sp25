cd958b03941411ac0e95f133562d3a90f1138d6f
Larry Wang <larrywang31@gmail.com>
2025-03-24 21:43:18 -0400

    start of proj

---
+++ labs/proj7/mymalloc.c
@@ -0,0 +1,44 @@
+#include "mymalloc.h"
+#include <unistd.h>
+
+/* 
+  Overview:
+
+*/
+
+/*
+  my_malloc()
+
+  - align s bytes of memory on an 8-byte quantity
+  - reserve 8 bytes before the pointer
+    - first 4 of 8 bytes stores the size of the chunk allocated
+      - users memory,
+      - bookkeeping bytes, and
+      - any padding
+  
+  Example:
+    my_malloc(9990) will pad 9990 to a multiple of eight (9992) and 
+    add 8 bytes for bookkeeping for a total of 10000 bytes. Since that
+    is bigger than 8192, call sbrk(10000). Put the number 10000 at 
+    address 0x10800 and return 0x10808 to the user.
+*/
+void *my_malloc(size_t size) {
+
+}
+
+void my_free(void *ptr) {
+
+}
+
+void *free_list_begin() {
+
+}
+
+void *free_list_next(void *node) {
+
+}
+
+void coalesce_free_list() {
+
+}
+
