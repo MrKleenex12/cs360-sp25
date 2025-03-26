ff2c927f3be6a41eeb8e679aa594b65f859141bf
Larry Wang <larrywang31@gmail.com>
2025-03-26 01:24:39 -0400

    added functions to malloc

--- labs/proj7/mymalloc.c
+++ labs/proj7/mymalloc.c
@@ -11,18 +11,39 @@ typedef struct flist {
   struct flist *blink;
 } *Flist;
 
-void print_Flist(Flist f) {
-  printf("loc: 0x%08lx s: %d f: 0x%08lx b: 0x%08lx\n", (UL)f, f->size, (UL)f->flink, (UL)f->blink);
+void print_Flist(Flist f, char *name) {
+  printf("%s: 0x%08lx s: %d f: 0x%08lx b: 0x%08lx\n", 
+    name, (UL)f, f->size, (UL)f->flink, (UL)f->blink);
 }
 
 /* Searches through list for chunk of memory that is big enough */
-void *find_chunk() {
-  return NULL;
+void *find_chunk(Flist h, size_t s) {
+  Flist f = h;
+  while(f != NULL) {
+    if(s <= f->size) { return f; }
+    f = f->flink;
+  }
+  return NULL;  /* return if no chunks found */
+}
+
+void *split(Flist *f, size_t s) {
+  void *ret;
+  int tmp;
+
+  /* Set up bookkeeping and memory to be returned */
+  tmp = (*f)->size;
+  (*f)->size = s;
+  ret = head+8;
+  /* Adjust head and empty memory */
+  (*f) = (Flist)(head += s);
+  (*f)->size = tmp - s; 
+
+  return ret;
 }
 
 void *my_malloc(size_t s) {
   Flist f;
-  void *ret;
+  void *ret = NULL;
   int tmp;
 
   /* Create heap if heap is null */
@@ -32,26 +53,40 @@ void *my_malloc(size_t s) {
     *h = 8192;
   }
   f = (Flist)head;
-  print_Flist(f);
+  print_Flist(f, "head");
   
-  /* Split memory chunk in two */
-  
-  /* Pad s to 8 bytes */
-  if((tmp = s % 8) != 0) { s += tmp; }
-  // printf("s: %zu\n", s);
-  /* Set up bookkeeping and memory to be returned */
-  tmp = f->size;
-  f->size = s += 8;
-  ret = head+8;
-  /* Adjust head and empty memory */
-  f = (Flist)(head += s);
-  f->size = tmp - s; 
+  /* Pad s to 8 bytes and add 8 for bookkeeping*/
+  if((tmp = s % 8) != 0) { s += (8-tmp); }
+  s += 8;
+  /* Find empty chunk of memory big enough for what is requested */
+  f = find_chunk(f, s);
+  if(f == NULL) { fprintf(stderr, "no chunks found\n"); exit(1); }
+  /* TODO Split memory chunk in two if bigger than requested */
+  ret = split(&f, s);
+
+  print_Flist(f, "new head");
+  printf("\n");
 
   return ret;
 }
 
 void my_free(void *ptr) {
+  /* new and old head nodes of Flist */
+  Flist new, old;
 
+  ptr -= 8;
+  printf("freeing: 0x%08lx\n", (UL)ptr);
+  old = (Flist)head;
+  head = ptr;
+  new = (Flist)head;
+
+  /* Adjust forward and backward pointers */
+  new->flink = old;
+  new->blink = NULL;
+  old->blink = new;
+
+  print_Flist(new, "new head");
+  print_Flist(old, "old head");
 }
 
 void *free_list_begin() {
