810d8e242c1743761d2758d8dc5c383d69a51dc8
Larry Wang <larrywang31@gmail.com>
2025-04-10 01:23:58 -0400

    add command functions:

--- labs/proj8/src/jshell.c
+++ labs/proj8/src/jshell.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include "fields.h"
 #include "dllist.h"
+#include "jval.h"
+
 
 /* Copied from Jantz lab 8 writeup */
-typedef struct {
-  char *stdin;          /* Filename from which to redirect stdin.  NULL if empty.*/ 
-  char *stdout;         /* Filename to which to redirect stdout.  NULL if empty.*/ 
+typedef struct{
+  char *stdinp;          /* Filename from which to redirect stdin.  NULL if empty.*/ 
+  char *stdoutp;         /* Filename to which to redirect stdout.  NULL if empty.*/ 
   int append_stdout;    /* Boolean for appending.*/ 
   int wait;             /* Boolean for whether I should wait.*/ 
   int n_commands;       /* The number of commands that I have to execute*/ 
@@ -16,8 +18,43 @@ typedef struct {
   Dllist comlist;       /* I use this to incrementally read the commands.*/ 
 } Command;
 
+
+void free_command(Command *com) {
+  if(com->stdinp != NULL) free(com->stdinp);
+  if(com->stdoutp != NULL) free(com->stdoutp);
+  if(com->argcs != NULL) free(com->argcs);
+  if(com->argvs != NULL) free(com->argvs);
+  free_dllist(com->comlist);
+}
+
+
+Command* make_command() {
+  Command *cm = (Command*)malloc(sizeof(Command));
+  cm->append_stdout = 0;
+  cm->wait = 1;
+  cm->n_commands = 0;
+  cm->stdinp = NULL;
+  cm->stdoutp = NULL;
+  cm->argcs = (int*)malloc(BUFSIZ);
+  cm->comlist = new_dllist();
+  return cm;
+}
+
+void move_argvs(char ***argvs,int processes, Dllist d) {
+  Dllist tmp;
+  int index = 0;
+  
+  // TODO allocate space for argvs
+  dll_traverse(tmp, d) {
+    argvs[index++] = (char**)tmp->val.v;
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   IS is = new_inputstruct(NULL);  // input proccessing
+  Command *com = make_command();
+  Dllist tmp;
 
   /*  Use char array for first commmand line argument
       set index to 1 if letter was found */
@@ -28,12 +65,27 @@ int main(int argc, char *argv[]) {
     letters[2] = strchr(argv[1], 'n') != NULL;
   }
 
-  /* TODO program one command execution */  
 
+  /* TODO program one command execution */  
+  while(get_line(is) > 0) {
+    if(strcmp(is->fields[0], "END") == 0) { break; }
+    else {
+      com->argcs[com->n_commands] = is->NF;
+      dll_append(com->comlist, new_jval_v((void*)is->fields));
+      com->n_commands++;
+    }
+  }
+
+
+  printf("number of commands = %d\n", com->n_commands);
+  /*
+  dll_traverse(tmp, com->comlist)
+    printf("%s ", tmp->)
+  */
   // while(get_line(is) > 0) {
   //   /* Ignore if blank line or line begins with a '#' */
   //   if(is->text1[0] == '#') continue;
-  //   else if(strcmp(is->text1, "END") == 0) {
+  //   else if(strcmp(is->fields[0], "END") == 0) {
   //     /*  TODO command is over should execute */ 
   //   }
   //   else if(is->text1[0] == '<') {
@@ -46,7 +98,7 @@ int main(int argc, char *argv[]) {
   //   else if(strcmp(is->fields[0], ">>") == 0) {
   //     /*  TODO redirect stdout of the last CPC and append to that file */ 
   //   }
-  //   else if(strcmp(is->text1, "NOWAIT") == 0) {
+  //   else if(strcmp(is->fields[0], "NOWAIT") == 0) {
   //     /*  TODO you will not wait for any of the child processes to exit */
   //   }
   //   else {
@@ -57,5 +109,6 @@ int main(int argc, char *argv[]) {
   // }
 
   jettison_inputstruct(is);
+  free_command(com);
   return 0;
 }
