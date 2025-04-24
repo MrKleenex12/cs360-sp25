e28a30eafc3999ec44bc202477caf33aa618824a
Larry Wang <larrywang31@gmail.com>
2025-04-24 13:56:33 -0400

    add oxygens to o_wait and print statements to show found atoms

--- labs/lab-a-bonding/bonding.c
+++ labs/lab-a-bonding/bonding.c
@@ -15,12 +15,12 @@ typedef struct global_info {
   Dllist o_wait;
 } global_info;
 
-typedef struct thread_mole {
+typedef struct molecule {
   pthread_cond_t *condition;
   int id;
-  int h1, h2;
+  int h1_atom, h2_atom;
   int o;
-} thread_mole;
+} molecule;
 
 void *initialize_v(char *verbosity) {
   global_info *g = (global_info *)malloc(sizeof(global_info));
@@ -31,57 +31,57 @@ void *initialize_v(char *verbosity) {
   return (void *)g;
 }
 
-thread_mole *make_tm() {
-  thread_mole *tm = (thread_mole *)malloc(sizeof(thread_mole));
-  tm->condition = new_cond();
-  tm->id = -1;
-  tm->h1 = -1;
-  tm->h2 = -1;
-  tm->o = -1;
+molecule *make_tm() {
+  molecule *m = (molecule *)malloc(sizeof(molecule));
+  m->condition = new_cond();
+  m->id = -1;
+  m->h1_atom = -1;
+  m->h2_atom = -1;
+  m->o = -1;
 
-  return tm;
+  return m;
 }
 
-thread_mole *get_thread(Dllist l) {
+molecule *get_atom(Dllist l) {
   Dllist temp = dll_first(l);
-  thread_mole* tm = (thread_mole*)temp->val.v;
+  molecule* m = (molecule*)temp->val.v;
   dll_delete_node(temp);
-  return tm;
+  return m;
 }
 
 void *hydrogen(void *arg) {
   struct bonding_arg *a;
   global_info *g;
-  thread_mole *tm;
+  molecule *m;
 
   a = (struct bonding_arg *)arg;
   g = (global_info *)a->v;
-  tm = make_tm();              /* Create a thread_mole data structure */
+  m = make_tm();              /* Create a molecule data structure */
   pthread_mutex_lock(g->lock); /* Lock the global mutex */
 
   /* Look at waiting lists and see if molecule can be created with two other
    * threads */
   if(!dll_empty(g->h_wait) && !dll_empty(g->o_wait)) {
-    thread_mole *other_h, *other_o;
+    molecule *h_atom, *o_atom;
 
     /* Find waiting h & waiting o, then remove from list */
-    other_h = get_thread(g->h_wait); 
-    other_o = get_thread(g->o_wait); 
+    h_atom = get_atom(g->h_wait); 
+    o_atom = get_atom(g->o_wait); 
 
-    printf("Found other two: %d & %d\n", other_h->id, other_o->id);
-    /* TODO Set ID's in all three thread_mole structs */
+    printf("Found other two: %d & %d\n", h_atom->id, o_atom->id);
+    /* TODO Set ID's in all three molecule structs */
 
     /* TODO Signal the other two threads */
 
     /* TODO Unlock mutex */
   } else {
     /* Add hydrogen molecule to h_wait and call pthread_cond_wait() */
-    tm->id = a->id;
-    dll_append(g->h_wait, new_jval_v((void *)tm));
-    printf("added %d to h_wait\n", tm->id);
+    m->id = a->id;
+    dll_append(g->h_wait, new_jval_v((void *)m));
+    printf("added %d to h_wait\n", m->id);
 
     /* Wait until signaled */
-    if(pthread_cond_wait(tm->condition, g->lock) != 0) {
+    if(pthread_cond_wait(m->condition, g->lock) != 0) {
       perror("wait failed");
       exit(1);
     }
@@ -93,35 +93,35 @@ void *hydrogen(void *arg) {
 void *oxygen(void *arg) {
   struct bonding_arg *a;
   global_info *g;
-  thread_mole *tm;
+  molecule *m;
 
   a = (struct bonding_arg *)arg;
   g = (global_info *)a->v;
-  tm = make_tm();              /* Create a thread_mole data structure */
+  m = make_tm();              /* Create a molecule data structure */
   pthread_mutex_lock(g->lock); /* Lock the global mutex */
 
   /* Checks if there are at least two waiting hydrogens in h_wait */
   if(dll_first(g->h_wait)->flink != g->h_wait) {
-    thread_mole *h1, *h2;
+    molecule *h1_atom, *h2_atom;
 
     /* Find two waiting hydrogens and remove from list */
-    h1 = get_thread(g->h_wait); 
-    h2 = get_thread(g->h_wait); 
+    h1_atom = get_atom(g->h_wait); 
+    h2_atom = get_atom(g->h_wait); 
 
-    printf("Found other two: %d & %d\n", h1->id, h2->id);
-    /* TODO Set ID's in all three thread_mole structs */
+    printf("Found other two: %d & %d\n", h1_atom->id, h2_atom->id);
+    /* TODO Set ID's in all three molecule structs */
 
     /* TODO Signal the other two threads */
 
     /* TODO Unlock mutex */
   } else {
     /* Add oxygen molecule to o_wait and call pthread_cond_wait() */
-    tm->id = a->id;
-    dll_append(g->o_wait, new_jval_v((void *)tm));
-    printf("added %d to o_wait\n", tm->id);
+    m->id = a->id;
+    dll_append(g->o_wait, new_jval_v((void *)m));
+    printf("added %d to o_wait\n", m->id);
 
     /* Wait until signaled */
-    if(pthread_cond_wait(tm->condition, g->lock) != 0) {
+    if(pthread_cond_wait(m->condition, g->lock) != 0) {
       perror("wait failed");
       exit(1);
     }
