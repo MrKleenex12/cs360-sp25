5d712e2ca44a67832aa03bca5966954c595a2ea3
Larry Wang <larrywang31@gmail.com>
2025-04-23 23:35:16 -0400

    write structures and hide testing dir

--- labs/lab-a-bonding/bonding.c
+++ labs/lab-a-bonding/bonding.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <pthread.h>
 #include "bonding.h"
 
-/* This solution compiles and hangs. */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void *initialize_v(char *verbosity)
-{
-  return NULL;
-}
+#include "dllist.h"
+#include "jval.h"
 
-void *hydrogen(void *arg)
-{
-  return NULL;
-}
+typedef struct global_info {
+  pthread_mutex_t *lock;
+  Dllist h_wait;
+  Dllist o_wait;
+} global_info;
 
-void *oxygen(void *arg)
-{
-  return NULL;
-}
+typedef struct thread_info {
+  pthread_cond_t *condition;
+  int id;
+  int h[2];
+  int o;
+} thread_info;
+
+void *initialize_v(char *verbosity) { return NULL; }
+
+void *hydrogen(void *arg) { return NULL; }
+
+void *oxygen(void *arg) { return NULL; }
