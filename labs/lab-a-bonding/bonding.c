e6265fb5d55955bf89abb04571e30c7d1648a192
Larry Wang <larrywang31@gmail.com>
2025-04-24 20:40:15 -0400

    finish bonding call, fix comments, and free

--- labs/lab-a-bonding/bonding.c
+++ labs/lab-a-bonding/bonding.c
@@ -63,102 +63,111 @@ void set_ids(molecule *h1, molecule *h2, molecule *o) {
   // clang-format on
 }
 
+void send_signals(pthread_cond_t *c1, pthread_cond_t *c2) {
+  if(pthread_cond_signal(c1) != 0) {
+    perror("H signal error:");
+    exit(1);
+  }
+  if(pthread_cond_signal(c2) != 0) {
+    perror("O signal error:");
+    exit(1);
+  }
+}
+
+/* Hydrogen thread function looks to find matching h and o atom to make a
+ * molecule. *arg is a bonding_arg value that holds custom structure as a
+ * void*. If cannot make a molecule, add hydrogen to h_wait list and call
+ * cond_wait() to wait for a match. If it can make a match, set the ids of each
+ * atom structure and let each h1, h2, and o atom call the same Bond(id1, id2,
+ * id3) three times */
+
 void *hydrogen(void *arg) {
-  struct bonding_arg *a;
-  struct global_info *g;
-  struct molecule *m;
-  char *result;
+  struct bonding_arg *a = (struct bonding_arg *)arg;
+  struct global_info *g = (global_info *)a->v;
+  struct molecule *m = make_molecule(a->id);
+  pthread_mutex_lock(g->lock);
 
-  a = (struct bonding_arg *)arg;
-  g = (global_info *)a->v;
-  m = make_molecule(a->id);    /* Create a molecule data structure with id */
-  pthread_mutex_lock(g->lock); /* Lock the global mutex */
-
-  /* Look at waiting lists and see if molecule can be created with two other
-   * threads */
+  /* Check O and H waitlists to find available atoms. If no match, add indiviual
+   * H atom to h_wait. Call cond_wait on condition variable */
   if(!dll_empty(g->h_wait) && !dll_empty(g->o_wait)) {
-    molecule *H, *O;
-
-    /* Find waiting h & waiting o, then remove from list */
-    H = get_atom(g->h_wait);
-    O = get_atom(g->o_wait);
-    printf("Found other two: %d & %d\n", H->id, O->id);
-
-    set_ids(m, H, O);
+    molecule *H = get_atom(g->h_wait);
+    molecule *O = get_atom(g->o_wait);
+    // printf("Found other two: %d & %d\n", H->id, O->id);
+    set_ids(m, H, O); /* Set the three thread ids of each molecule struct */
     print(m, H, O);
 
     if(pthread_cond_signal(H->condition) != 0) {
-      perror("H signal error:");
+      perror("H: H signal error:");
       exit(1);
     }
     if(pthread_cond_signal(O->condition) != 0) {
-      perror("H signal error:");
+      perror("H: O signal error:");
       exit(1);
     }
-
     pthread_mutex_unlock(g->lock);
   } else {
-    /* Add hydrogen molecule to h_wait and call pthread_cond_wait() */
     dll_append(g->h_wait, new_jval_v((void *)m));
-    printf("added %d to h_wait\n", m->id);
-
-    /* Wait until signaled */
+    // printf("added %d to h_wait\n", m->id);
     if(pthread_cond_wait(m->condition, g->lock) != 0) {
-      perror("wait failed");
+      perror("H: wait failed");
       exit(1);
     }
     pthread_mutex_unlock(g->lock);
   }
 
-  /* TODO call Bond and return result */
-  return NULL;
+  /* Return Bond(), free m and m->condition*/
+  char *rv = Bond(m->h[0], m->h[1], m->o);
+  if(pthread_cond_destroy(m->condition) != 0) {
+    perror("H: destroy condition error:");
+    exit(1);
+  }
+  free(m);
+  return rv;
 }
 
+/* Oxygen thread function does the same thing as hydrogen() except it tries to
+ * find two matching hydrogen atoms. Same logic in order to call Bond() */
+
 void *oxygen(void *arg) {
-  struct bonding_arg *a;
-  struct global_info *g;
-  struct molecule *m;
-  char *result;
+  struct bonding_arg *a = (struct bonding_arg *)arg;
+  struct global_info *g = (global_info *)a->v;
+  struct molecule *m = make_molecule(a->id);
+  pthread_mutex_lock(g->lock);
 
-  a = (struct bonding_arg *)arg;
-  g = (global_info *)a->v;
-  m = make_molecule(a->id);    /* Create a molecule data structure with id */
-  pthread_mutex_lock(g->lock); /* Lock the global mutex */
-
-  /* Checks if there are at least two waiting hydrogens in h_wait */
+  /* Check H waitlist to find available H atoms. If no match, add indiviual
+   * O atom to o_wait. Call cond_wait on condition variable */
   if(dll_first(g->h_wait)->flink != g->h_wait) {
-    molecule *H1, *H2;
-
-    /* Find two waiting hydrogens and remove from list */
-    H1 = get_atom(g->h_wait);
-    H2 = get_atom(g->h_wait);
-    printf("Found other two: %d & %d\n", H1->id, H2->id);
-
-    set_ids(H1, H2, m);
+    molecule *H1 = get_atom(g->h_wait);
+    molecule *H2 = get_atom(g->h_wait);
+    // printf("Found other two: %d & %d\n", H1->id, H2->id);
+    set_ids(H1, H2, m); /* Set the three thread ids of each molecule struct */
     print(H1, H2, m);
 
     if(pthread_cond_signal(H1->condition) != 0) {
-      perror("H signal error:");
+      perror("O: H1 signal error:");
       exit(1);
     }
     if(pthread_cond_signal(H2->condition) != 0) {
-      perror("H signal error:");
+      perror("O: H2 signal error:");
       exit(1);
     }
     pthread_mutex_unlock(g->lock);
   } else {
-    /* Add oxygen molecule to o_wait and call pthread_cond_wait() */
     dll_append(g->o_wait, new_jval_v((void *)m));
-    printf("added %d to o_wait\n", m->id);
-
-    /* Wait until signaled */
+    // printf("added %d to o_wait\n", m->id);
     if(pthread_cond_wait(m->condition, g->lock) != 0) {
-      perror("wait failed");
+      perror("O: wait failed");
       exit(1);
     }
     pthread_mutex_unlock(g->lock);
   }
-  /* TODO call Bond and return result */
 
-  return NULL;
+  /* Return Bond(), free m and m->condition*/
+  char *rv = Bond(m->h[0], m->h[1], m->o);
+  if(pthread_cond_destroy(m->condition) != 0) {
+    perror("O: destroy condition error:");
+    exit(1);
+  }
+  free(m);
+  return rv;
 }
