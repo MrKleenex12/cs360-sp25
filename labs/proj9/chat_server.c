258a3df0dbd61ff87b23abfbc172332696487c17
Larry Wang <larrywang31@gmail.com>
2025-05-03 17:06:16 -0400

    proj9: start chat_server.c with basics

--- labs/proj9/chat_server.c
+++ labs/proj9/chat_server.c
@@ -1,1 +1,29 @@
-int main() { return 0; }
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dllist.h"
+#include "fields.h"
+#include "jrb.h"
+#include "jval.h"
+#include "sockettome.h"
+
+int main(int argc, char **argv) {
+  char *hn;
+  int fd, port, sock;
+  FILE *fin, *fout;
+  FILE *stdin_to_socket[2];
+  FILE *socket_to_stdin[2];
+  pthread_t tid;
+
+  if(argc < 3) {
+    fprintf(stderr, "usage: ./chat_server port Chat-Room-Names ...\n");
+    exit(1);
+  }
+  if((port = atoi(argv[1])) < 8000) {
+    fprintf(stderr, "port must be at least 8000\n");
+    exit(1);
+  }
+
+  return 0;
+}
