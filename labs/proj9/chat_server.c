f296f10e428f02e95601cdf99a4915877d9e2419
Larry Wang <larrywang31@gmail.com>
2025-05-05 15:49:49 -0400

    proj9: make usage function

--- labs/proj9/chat_server.c
+++ labs/proj9/chat_server.c
@@ -10,24 +10,39 @@
 #include "jval.h"
 #include "sockettome.h"
 
+/* Error check usage and CL arguments */
+void usage(int argc, char **argv, int *port) {
+  if(argc < 3 || (*port = atoi(argv[1])) < 8000) {
+    fprintf(
+        stderr,
+        "usage: ./chat_server port_num(at least 8000) Chat-Room-Names ...\n");
+    exit(1);
+  }
+}
 
 int main(int argc, char **argv) {
   char *hn;
   void *rv;
-  int fd, port, *sock;
+  int fd, port, sock;
   // FILE *fin, *fout;
   // FILE *stdin_to_socket[2];
   // FILE *socket_to_stdin[2];
   pthread_t tid;
 
-  if(argc < 3) {
-    fprintf(stderr, "usage: ./chat_server port Chat-Room-Names ...\n");
-    exit(1);
-  }
-  if((port = atoi(argv[1])) < 8000) {
-    fprintf(stderr, "port must be at least 8000\n");
+  /* Error check usage and CL arguments */
+  usage(argc, argv, &port);
+  sock = serve_socket(port);
+
+  /*
+  if(pthread_create(&tid, NULL, main_thread, (void *)sock) != 0) {
+    perror("main pthread create: ");
     exit(1);
   }
 
+  if(pthread_join(tid, &rv) != 0) {
+    perror("main pthread join: ");
+    exit(1);
+  }
+  */
   return 0;
 }
