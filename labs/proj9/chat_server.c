65ce7a4518b0cf2010a7d76af7c5c031dd6a1173
Larry Wang <larrywang31@gmail.com>
2025-05-05 21:09:07 -0400

    proj9: allocate Rooms and JRB for rooms

--- labs/proj9/chat_server.c
+++ labs/proj9/chat_server.c
@@ -10,9 +10,16 @@
 #include "jval.h"
 #include "sockettome.h"
 
+typedef struct Room {
+  char *name;
+  Dllist clients;
+  Dllist messages;
+} Room;
+
 typedef struct Client {
   FILE *stdin_to_socket[2];
   FILE *socket_to_stdout[2];
+  Room *r;
 } Client;
 
 Client *make_client(int fd) {
@@ -20,6 +27,7 @@ Client *make_client(int fd) {
   FILE *fin = fdopen(fd, "r");
   FILE *fout = fdopen(fd, "w");
 
+  c->r = NULL;
   c->stdin_to_socket[0] = stdin;
   c->stdin_to_socket[1] = fout;
   c->socket_to_stdout[0] = fin;
@@ -52,15 +60,16 @@ void *process_connection(void *c) {
   exit(0);
 }
 
-void *one_person(void *arg) { return NULL; }
+/* Called by main_thread to set up communication */
+void *client_thread() { return NULL; }
 
-/* Main Thread spins off 1 thread to connect to one person and keep talking */
+/* Loop indefinitely to accept & spawn client threads */
 void *main_thread(void *s) {
   Client *c;
   pthread_t tid;
   char buf[BUFSIZ], *hn;
   int sock, fd;
-  
+
   sock = *((int *)s);
   fd = accept_connection(sock);
   c = make_client(fd);
@@ -72,32 +81,59 @@ void *main_thread(void *s) {
   sprintf(buf, "Recieving from Server: %s\n", hn);
   write(fd, buf, strlen(buf));
 
-  if(pthread_create(&tid, NULL, process_connection, (void*) c->socket_to_stdout) != 0) {
+  if(pthread_create(&tid, NULL, process_connection,
+                    (void *)c->socket_to_stdout) != 0) {
     perror("main_thread pcreate:");
     exit(1);
-  } 
+  }
 
-  (void) process_connection(c->stdin_to_socket); 
+  (void)process_connection(c->stdin_to_socket);
   exit(0);
 }
 
 int main(int argc, char **argv) {
-  int port, sock;
+  JRB rooms;
+  Room *room_arr;
   pthread_t tid;
+  char *name;
+  int i, port, sock;
   void *rv;
 
-  /* Error check usage and CL arguments */
-  usage(argc, argv, &port);
-  sock = serve_socket(port);
+  usage(argc, argv, &port); /* Error check usage and set port num */
 
-  if(pthread_create(&tid, NULL, main_thread, (void *)&sock) != 0) {
-    perror("main pthread create: ");
-    exit(1);
+  sock = serve_socket(port); /* Set up socket with the given port */
+
+  /* Make JRB and allocate memory for all Room pointers */
+  rooms = make_jrb();
+  room_arr = (Room *)malloc(sizeof(Room) * (argc - 2));
+  /* Insert each Room into JRB and set up dllists */
+  for(i = 2; i < argc; i++) {
+    name = strdup(argv[i]);
+
+    (room_arr + (i - 2))->clients = new_dllist();
+    (room_arr + (i - 2))->messages = new_dllist();
+    (room_arr + (i - 2))->name = name;
+    jrb_insert_str(rooms, name, new_jval_v((void *)room_arr + (i - 2)));
   }
 
-  if(pthread_join(tid, &rv) != 0) {
-    perror("main pthread join: ");
-    exit(1);
+  // /* Call main thread to start accepting clients */
+  // if(pthread_create(&tid, NULL, main_thread, (void *)&sock) != 0) {
+  //   perror("main pthread create: ");
+  //   exit(1);
+  // }
+
+  // if(pthread_join(tid, &rv) != 0) {
+  //   perror("main pthread join: ");
+  //   exit(1);
+  // }
+
+  /* Freeing everything */
+  for(i = 0; i < argc - 2; i++) {
+    free_dllist((room_arr + i)->clients);
+    free_dllist((room_arr + i)->messages);
+    free((room_arr + i)->name);
   }
+  free(room_arr);
+  jrb_free_tree(rooms);
   return 0;
 }
